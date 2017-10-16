6a8fe406f3e1fe1e1ea747d440431e364966de21
Jason Würtz <jwuertz@upei.ca>
2017-10-16 12:17:09 -0300

    refactor: split into Preprocessing and DrawBoundingBox functions

--- OpenCV-3.3.0-StarterKit/Examples/C# (Test)/ShaprCVTest/ShaprCVTest/Program.cs
+++ OpenCV-3.3.0-StarterKit/Examples/C# (Test)/ShaprCVTest/ShaprCVTest/Program.cs
@@ -19,31 +19,36 @@ namespace ShaprCVTest
 		public static void DetectCups()
 		{
 			string windowName = "Cup Detector";
-
+			Size size = new Size(500, 500);
 
 			//Load image
 			Mat input_image = CvInvoke.Imread("..\\..\\Images\\Cups.jpg", LoadImageType.AnyColor);
+			Image<Hsv, Byte> preprocessed_image = new Image<Hsv, Byte>(size);
+			Preprocess(input_image, preprocessed_image, size);
+			//blur the image to reduce noise and smoothe edges
+			CvInvoke.GaussianBlur(preprocessed_image, preprocessed_image, new Size(31, 31), 0, 0);
 
-			//Resize image
-			Size size = new Size(500, 500);
-			Image<Bgr, Byte> resized_image = new Image<Bgr, Byte>(size);
-			CvInvoke.Resize(input_image, resized_image, size);
-
-
-			CvInvoke.GaussianBlur(resized_image, resized_image, new Size(31, 31), 0, 0);
-			Image<Hsv, Byte> hsv_image = new Image<Hsv, Byte>(size);
-			Image<Gray, Byte> filtered_image = new Image<Gray, Byte>(size);
-
-			CvInvoke.CvtColor(resized_image, hsv_image, ColorConversion.Bgr2Hsv);
 
 			ScalarArray lower = new ScalarArray(new Hsv(0, 0, 0).MCvScalar);
 			ScalarArray upper = new ScalarArray(new Hsv(90, 255, 255).MCvScalar);
+			Image<Gray, Byte> filtered_image = new Image<Gray, Byte>(size);
+			CvInvoke.InRange(preprocessed_image, lower, upper, filtered_image);
 
+			Image<Bgr, Byte> output_image = new Image<Bgr, Byte>(size);
+			DrawBoundingBoxes(filtered_image, output_image);
 
-			CvInvoke.InRange(hsv_image, lower, upper, filtered_image);
+			CvInvoke.Imshow(windowName, output_image);
+			//CvInvoke.Imshow(windowName, contours_image);
+			//Wait for the key pressing event
+			//Destroy the window if key is pressed
+			CvInvoke.WaitKey(0);
+			CvInvoke.DestroyWindow(windowName);
 
+		}
 
-			Image<Gray, float> laplace_image = filtered_image.Laplace(3);
+		private static void DrawBoundingBoxes(Image<Gray, byte> input, Image<Bgr, byte> output)
+		{
+			Image<Gray, float> laplace_image = input.Laplace(3);
 			Image<Gray, float> erode_image = laplace_image.Erode(2);
 			Image<Gray, Byte> byteErode_image = erode_image.Convert<Gray, Byte>();
 			Image<Gray, Byte> thresholded_image = byteErode_image.ThresholdToZero(new Gray(240));
@@ -57,9 +62,8 @@ namespace ShaprCVTest
 			Console.Write(tree);
 
 			Bgr bgrRed = new Bgr(Color.Red);
-			Image<Bgr, Byte> output_image = new Image<Bgr, Byte>(size);
 
-			CvInvoke.CvtColor(filtered_image, output_image, ColorConversion.Gray2Bgr);
+			CvInvoke.CvtColor(input, output, ColorConversion.Gray2Bgr);
 
 			for (int i = 0; i < contours.Size; i++)
 			{
@@ -67,16 +71,17 @@ namespace ShaprCVTest
 				if ((box.Width < 400 && box.Height < 400) &&
 					(box.Width > 20 && box.Height > 20))
 				{
-					output_image.Draw(box, bgrRed, 2);
+					output.Draw(box, bgrRed, 2);
 				}
 			}
+		}
 
-			CvInvoke.Imshow(windowName, output_image);
-			//CvInvoke.Imshow(windowName, contours_image);
-			//Wait for the key pressing event
-			//Destroy the window if key is pressed
-			CvInvoke.WaitKey(0);
-			CvInvoke.DestroyWindow(windowName);
+		private static void Preprocess(Mat input, Image<Hsv, Byte> output, Size size) { 
+
+			//Resize image
+			Image<Bgr, Byte> resized_image = new Image<Bgr, Byte>(size);
+			CvInvoke.Resize(input, resized_image, size);
+			CvInvoke.CvtColor(resized_image, output, ColorConversion.Bgr2Hsv);
 
 		}
 	}
