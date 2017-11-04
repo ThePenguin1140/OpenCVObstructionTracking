d38c461417486317f37008e45d5ad770334d3761
Agon <Jawad_Jandaly@hotmail.com>
2017-11-04 19:26:58 -0300

    feat(Video): draw Contours on pre-recorded video

--- CV_Main.cs
+++ CV_Main.cs
@@ -6,7 +6,10 @@ namespace ShaprCVTest
   {
     public static void Main()
     {
-      CV_Program.DetectCups_Image ("..\\..\\Images\\Cups1.jpg");
+      //CV_Program.DetectCups_Image ("..\\..\\Images\\Cups1.jpg");
+      //CV_Program.DetectCups_Video ("..\\..\\Videos\\Cups.mp4", true, true);
+
+      CV_Program.DetectCups_Video ();
     }
   }
 }
--- CV_Program.cs
+++ CV_Program.cs
@@ -29,18 +29,67 @@ namespace ShaprCVTest
       CvInvoke.DestroyAllWindows();
     }
 
-    public static void DetectCups_Video( string VidPath = "..\\..\\Videos\\Cups.avi", bool ShowHSV = false, bool ShowGray = false) 
+    public static void DetectCups_Video( string VidPath = "..\\..\\Videos\\Cups.mp4", bool ShowHSV = false, bool ShowGray = false) 
     {
       Console.WriteLine( "CV_Program: DetectCups_Video(): [" + ShowHSV + ", " + ShowGray + "] " + VidPath + "" );
 
       CV_Program.ShowHSV  = ShowHSV ;
       CV_Program.ShowGray = ShowGray;
 
-      /*
-       * 
-       * INCOMPLETE ! ! ! 
-       * 
-       */
+      // Initialize video capture from the video file and check if it worked.
+      Capture vidCap = new Capture( VidPath );
+
+      if ( vidCap.Grab())
+      {
+        Console.WriteLine( "CV_Program: DetectCups_Video(): Successfully opened video file" );
+      }
+      else
+      {
+        Console.WriteLine( "CV_Program: DetectCups_Video(): Failed to open video file" );
+        return;
+      }
+
+      // Create a window and give it a name.
+      string wiName = "DetectCups_Video";
+      CvInvoke.NamedWindow( wiName );
+
+      // This matrix will contain our image.
+      Mat frame = new Mat();
+
+      //The Main Loop: Instead of while(true)
+      for ( int TimeOut = 0; TimeOut< 10000; TimeOut++ )
+      {
+        // Read a video frame into our image
+        // If we get an empty frame, we abort because have reached the end of the video stream
+        vidCap.Grab();
+        vidCap.Retrieve( frame );
+        if ( frame.IsEmpty ) break;
+
+        // Make sure the image is a 3-channel 24-bit image
+        if ( !( frame.Depth == DepthType.Cv8U ) && frame.NumberOfChannels == 3 )
+        {
+          Console.WriteLine( "CV_Program: DetectCups_Video(): Unexpected image format!" );
+          Console.WriteLine( "CV_Program: DetectCups_Video(): Type [" + frame.GetType().ToString() + "] and Channels [" + frame.NumberOfChannels + "]" );
+          return;
+        }
+
+        // Show the image in the window
+        CvInvoke.Imshow( wiName, DetectCups(frame) );
+
+        // Quit the loop when the Esc key is pressed.
+        // Calling waitKey is important, even if you're not interested in keyboard input!
+        int keyPressed = CvInvoke.WaitKey( 1 );
+
+        if ( keyPressed != -1 && keyPressed != 255 )
+        {
+          // Only the least-significant 16 bits contain the actual key code. The other bits contain modifier key states
+          keyPressed &= 0xFFFF;
+          Console.WriteLine( "CV_Program: DetectCups_Video(): Key pressed: " + keyPressed );
+          if ( keyPressed == 27 ) break;
+        }
+      }
+
+      Console.WriteLine( "CV_Program: DetectCups_Video(): Ended Video Function." );
     }
 
     public static Image<Bgr, byte> DetectCups( Mat input_image )
@@ -48,16 +97,16 @@ namespace ShaprCVTest
     	Size size = new Size( 700, 700 );
 
     	//Load image
-    	Image<Gray, byte> preprocessed_image = new Image<Gray, byte>( size );
+      Image<Gray, byte> preprocessed_image = new Image<Gray, byte>( size );
 
-    	//denoise, smoothe and threshold
-    	Preprocess( input_image, preprocessed_image, size );
+      //denoise, smoothe and threshold
+      Preprocess( input_image, preprocessed_image, size );
 
     	Image<Bgr, byte> output_image = new Image<Bgr, byte>( input_image.Size );
     	output_image = input_image.ToImage<Bgr, byte>();
     	CvInvoke.Resize( output_image, output_image, size );
 
-    	DrawContours( output_image, GetContours( preprocessed_image ) );
+      DrawContours( output_image, GetContours( preprocessed_image ), output_image.Mat );
 
     	return output_image;
     }
@@ -89,19 +138,26 @@ namespace ShaprCVTest
     	return contours;
     }
 
-    private static void DrawContours( Image<Bgr, byte> output, VectorOfVectorOfPoint contours )
+    private static void DrawContours( Image<Bgr, byte> output, VectorOfVectorOfPoint contours, Mat frame = null )
     {
     	Bgr bgrRed = new Bgr( Color.Red );
+
+      int boxID = 0;
 
     	for ( int i = 0; i < contours.Size; i++ )
       {
     		Rectangle box = CvInvoke.BoundingRectangle( contours[i] );
 
-    		if ( ( box.Width  < 400 && box.Height < 400 ) &&
-    			   ( box.Width  > 50  && box.Height > 50  ) &&
-    			   ( box.Height > box.Width ) ) 
+    		if ( ( box.Width  < 400       && box.Height     < 400 ) &&
+    			   ( box.Width  > 50        && box.Height     > 50  ) &&
+             ( box.Height > box.Width && box.Location.Y > 100 ) ) 
              {
                 output.Draw( box, bgrRed, 2 );
+
+                boxID++;
+
+                if ( frame != null )
+                CvInvoke.PutText( frame, "[" + boxID + "]", new System.Drawing.Point( box.Location.X + 5, box.Location.Y - 10 ), FontFace.HersheyPlain, 1.25, new MCvScalar( 255, 0, 255 ), 2 );
     		     }
     	}
     }
@@ -113,8 +169,11 @@ namespace ShaprCVTest
     	Image<Bgr, byte> resized_image = new Image<Bgr, byte>( size );
     	CvInvoke.Resize( input, resized_image, size );
 
-    	CvInvoke.FastNlMeansDenoisingColored( resized_image, resized_image, 3, 3, 7, 21 );
-    	resized_image = resized_image.SmoothMedian( 15 );
+      //Causes a lot of lag between frames
+    	//CvInvoke.FastNlMeansDenoisingColored( resized_image, resized_image, 3, 3, 7, 21 );
+
+      //Causes a bit of lag between frames
+      resized_image = resized_image.SmoothGaussian( 15 );
     	resized_image._GammaCorrect( 2d );
     	resized_image._EqualizeHist();
 
