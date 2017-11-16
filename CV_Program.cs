fea6d7cd38352be5ba0b6373d2b029e1632af910
Jason Würtz <jwuertz@upei.ca>
2017-11-16 14:02:22 -0400

    feat(threading): start basic threading setup [DOESN'T WORK]

--- CV_Program.cs
+++ CV_Program.cs
@@ -11,8 +11,15 @@ namespace ShaprCVTest
   {
     public static bool      TrackCups = false;
     public static CV_Cup[]  Cups             ;
-    public static bool ShowHSV  = false;
-    public static bool ShowFiltered = false;
+    public static bool      ShowHSV  = false;
+    public static bool      ShowFiltered = false;
+
+    private static Capture vidCap;
+    private static Image<Bgr, byte> frame = null;
+
+    private static bool running = false;
+    private static double frameNum = 0d;
+    private static Object lockObject = new Object();
 
     public static void DetectCups_Image( string ImgPath = "..\\..\\Images\\Cups.jpg", bool ShowHSV = false, bool ShowGray = false) 
     {
@@ -33,77 +40,60 @@ namespace ShaprCVTest
 
     public static void DetectCups_Video( string VidPath = "..\\..\\Videos\\Cups.mp4", bool ShowHSV = false, bool ShowGray = false) 
     {
+      double totalFrames = 0d;
+
       Console.WriteLine( "CV_Program: DetectCups_Video(): [" + ShowHSV + ", " + ShowGray + "] " + VidPath + "" );
 
       CV_Program.ShowHSV  = ShowHSV ;
       CV_Program.ShowFiltered = ShowGray;
 
+      CvInvoke.NamedWindow( "Video" );
+
       // Initialize video capture from the video file and check if it worked.
-      Capture vidCap = new Capture( VidPath );
-
-      if ( vidCap.Grab())
-      {
-        Console.WriteLine( "CV_Program: DetectCups_Video(): Successfully opened video file" );
-      }
-      else
-      {
-        Console.WriteLine( "CV_Program: DetectCups_Video(): Failed to open video file" );
-        return;
-      }
-
-      // Create a window and give it a name.
-      string wiName = "DetectCups_Video";
-      CvInvoke.NamedWindow( wiName );
-
-      bool paused = false;
-
-      // This matrix will contain our image.
-      Mat frame = new Mat();
-      double frameNum = vidCap.GetCaptureProperty( CapProp.PosFrames );
-      double nextFrame = frameNum;
-      frame = vidCap.QueryFrame();
-
-      //The Main Loop: Instead of while(true)
-      for ( int TimeOut = 0; TimeOut< 10000; TimeOut++ ) {
-        // Read a video frame into our image
-        // If we get an empty frame, we abort because have reached the end of the video stream
-        if ( frame.IsEmpty ) break;
-
-        // Make sure the image is a 3-channel 24-bit image
-        if ( !( frame.Depth == DepthType.Cv8U ) && frame.NumberOfChannels == 3 ) {
-          Console.WriteLine( "CV_Program: DetectCups_Video(): Unexpected image format!" );
-          Console.WriteLine( "CV_Program: DetectCups_Video(): Type [" + frame.GetType().ToString() + "] and Channels [" + frame.NumberOfChannels + "]" );
-          return;
-        }
-
-        // Show the image in the window
-        CvInvoke.Imshow( wiName, DetectCups(frame) );
-
-        // Quit the loop when the Esc key is pressed.
-        // Calling waitKey is important, even if you're not interested in keyboard input!
-        int keyPressed = CvInvoke.WaitKey( 1 );
-        nextFrame = vidCap.GetCaptureProperty( CapProp.PosFrames );
-        if ( paused ) nextFrame = vidCap.GetCaptureProperty( CapProp.PosFrames ) - 1;
-
-        if ( keyPressed != -1 && keyPressed != 255 ) {
-            // Only the least-significant 16 bits contain the actual key code. The other bits contain modifier key states
-            keyPressed &= 0xFFFF;
-            Console.WriteLine( "CV_Program: DetectCups_Video(): Key pressed: " + keyPressed );
-          if ( keyPressed == 27 ) break;
-          else if ( keyPressed == 116 ) InitCupTracking( frame );
-          else if ( keyPressed == 97 && nextFrame > 0 && paused ) {
-            //prev 
-            nextFrame -= 1;
-          } else if ( keyPressed == 100 && nextFrame < vidCap.GetCaptureProperty( CapProp.FrameCount ) && paused ) {
-            //next
-            nextFrame += 1;
-          } else if ( keyPressed == 32 ) paused = !paused;
-        }
-        vidCap.SetCaptureProperty( CapProp.PosFrames, nextFrame );
-        frame = vidCap.QueryFrame();
+      try {
+        vidCap = new Capture( VidPath );
+        vidCap.ImageGrabbed += new EventHandler(ProcessFrame);
+        totalFrames = vidCap.GetCaptureProperty( CapProp.FrameCount );
+        vidCap.Start();
+        Console.WriteLine( "Started capture" );
+        running = true;
+      } catch ( NullReferenceException e ) {
+        Console.WriteLine( e.Message );
       }
 
-      Console.WriteLine( "CV_Program: DetectCups_Video(): Ended Video Function." );
+      while ( running && totalFrames > frameNum ) {
+        if( frame != null) CvInvoke.Imshow( "Video", frame );
+      }
+
+      vidCap.Stop();
+      CvInvoke.WaitKey( 0 );
+      CvInvoke.DestroyAllWindows();
+    }
+
+    static void ProcessFrame( object sender, EventArgs e ) {
+      Console.WriteLine( "spawn" );
+      double curFrame = 0d;
+      Mat myInputFrame = null;
+
+      Capture _cap = (Capture)sender;
+
+      Image<Bgr, byte> myOutputFrame = null;
+      lock ( lockObject ) {
+        curFrame = _cap.GetCaptureProperty( CapProp.PosFrames );
+        myInputFrame = _cap.QueryFrame();
+      }
+
+      if ( myInputFrame.IsEmpty ) return;
+
+      //process myFrame
+      myOutputFrame = DetectCups( myInputFrame );
+
+      lock ( lockObject ) {
+        if ( curFrame > frameNum ) {
+          frameNum = curFrame;
+          frame = myOutputFrame;
+        } 
+      }
     }
 
     public static Image<Bgr, byte> DetectCups( Mat input_image )
