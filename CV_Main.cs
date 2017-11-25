6b616883640d94e1e20be5266ccea03b21b1d24f
Jason Würtz <jwuertz@upei.ca>
2017-11-25 13:32:30 -0400

    fix(main): use new test video

--- CV_Main.cs
+++ CV_Main.cs
@@ -8,7 +8,7 @@ namespace ShaprCVTest
     {
       //CV_Program.DetectCups_Image ("..\\..\\Images\\Cups1.jpg");
       //CV_Program.DetectCups_Video ("..\\..\\Videos\\Cups.mp4", true, true);
-      CV_Program.DetectCups_Video ("..\\..\\Videos\\Cups_Cut.mp4");
+      CV_Program.DetectCups_Video ("..\\..\\Videos\\Sample2.mp4", true, true);
 
       //CV_Program.DetectCups_Video ();
     }
