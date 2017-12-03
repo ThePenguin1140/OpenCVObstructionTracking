17a64f73c5f0779cd35248063ffc15d7e0820d15
Jason Würtz <jwuertz@upei.ca>
2017-12-02 20:19:09 -0400

    test(main): add test code for mov dir

--- CV_Main.cs
+++ CV_Main.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace ShaprCVTest
 {
@@ -8,9 +12,16 @@ namespace ShaprCVTest
     {
       //CV_Program.DetectCups_Image ("..\\..\\Images\\Cups1.jpg");
       //CV_Program.DetectCups_Video ("..\\..\\Videos\\Cups.mp4", true, true);
-      CV_Program.DetectCups_Video ("..\\..\\Videos\\Sample3.mp4");
+//      CV_Program.DetectCups_Video ("..\\..\\Videos\\Sample5.mp4", true, true);
 
       //CV_Program.DetectCups_Video ();
+      
+      Mat test_image = CvInvoke.Imread( "..\\..\\Images\\CornerTest1.jpg", LoadImageType.AnyColor );
+      Rectangle roi = new Rectangle( 0, 0, test_image.Width, test_image.Height );
+      int result = PreProcessing.MovementDirection(test_image.ToImage<Gray, byte>(), roi);
+      
+      Console.WriteLine( result );
+      
     }
   }
 }
