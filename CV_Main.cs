8a7b40e0d6b7ac0f96f7db55f0989c58e10b6ff7
Jason Würtz <jwuertz@upei.ca>
2017-12-02 21:43:34 -0400

    feat(main): add tests for move directions

--- CV_Main.cs
+++ CV_Main.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Shape;
 using Emgu.CV.Structure;
 
 namespace ShaprCVTest
@@ -15,13 +17,41 @@ namespace ShaprCVTest
 //      CV_Program.DetectCups_Video ("..\\..\\Videos\\Sample5.mp4", true, true);
 
       //CV_Program.DetectCups_Video ();
-      
-      Mat test_image = CvInvoke.Imread( "..\\..\\Images\\CornerTest1.jpg", LoadImageType.AnyColor );
-      Rectangle roi = new Rectangle( 0, 0, test_image.Width, test_image.Height );
-      int result = PreProcessing.MovementDirection(test_image.ToImage<Gray, byte>(), roi);
-      
-      Console.WriteLine( result );
-      
+
+      testDirections();
+
+    }
+
+    private static void testDirections()
+    {
+      string imageFolder = "..\\..\\Images\\directionTestFiles";
+      DirectoryInfo d = new DirectoryInfo(imageFolder);
+      int count = 0;
+      int passes = 0;
+      foreach (FileInfo file in d.GetFiles("*"))
+      {
+        count++;
+        string fileName = file.Name;
+        string expectedResult = fileName.Split('_')[1].Split('.')[0].Trim();
+        Mat testFile = CvInvoke.Imread(file.FullName, LoadImageType.Grayscale);
+        Rectangle roi = new Rectangle( 0, 0, testFile.Width, testFile.Height );
+        int result = PreProcessing.MovementDirection(testFile.ToImage<Gray, byte>(), roi);
+
+        if (result == 1 && expectedResult == "right")
+        {
+          passes++;
+          Console.WriteLine("PASS | " + fileName);
+        }
+        else if (result == -1 && expectedResult == "left")
+        {
+          passes++;
+          Console.WriteLine("PASS | " + fileName);
+        }
+        else Console.WriteLine("FAIL ---- Expected | " + expectedResult + " Recieved | " + result);
+      }
+        
+      Console.Write("____________________________________________________________________\n" +
+                    "" + passes + "/" + count + " of tests passed.");
     }
   }
 }
